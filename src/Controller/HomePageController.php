f2086aa768603a62218880e06e95d5649ef2d34f
Nathan.P <nathan.poncet@crea-inseec.com>
2023-01-30 12:20:25 +0100

    :zap: add components

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
-    #[Route('/', name: 'app_home_page')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('pages/home.html.twig', [
