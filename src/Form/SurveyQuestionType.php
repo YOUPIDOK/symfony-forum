b62afa27e84314b981f6e12043f0bb1f20eb2d7e
Nathan.P <nathan.poncet@crea-inseec.com>
2023-01-31 12:30:23 +0100

    :zap: add components

--- src/Form/SurveyQuestionType.php
+++ src/Form/SurveyQuestionType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Survey;
 use App\Entity\SurveyQuestion;
+use App\Enum\SurveyQuestionTypeEnum;
+use App\Enum\UserRoleEnum;
+use App\Form\CustomType\EntitySelectChoicesType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +18,16 @@ class SurveyQuestionType extends AbstractType
     {
         $builder
             ->add('question')
-            ->add('type')
-            ->add('survey')
-        ;
+            ->add('type', ChoiceType::class, [
+                'label' => 'Type de question',
+                'required' => true,
+                'choices' => SurveyQuestionTypeEnum::getChoices()
+            ])
+            ->add('survey', EntitySelectChoicesType::class, [
+                'label' => 'Formulaire',
+                'class' => Survey::class,
+                'required' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
