8f937d053e3897b2dbe169f082dfb93be258baea
Nathan.P <nathan.poncet@crea-inseec.com>
2023-01-31 09:57:55 +0100

    :zap: add components

---
+++ src/Form/WorkshopSectorType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\WorkshopSector;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class WorkshopSectorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'nom',
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'description',
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => WorkshopSector::class,
+        ]);
+    }
+}
