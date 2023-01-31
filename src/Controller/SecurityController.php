d66b5437acc25bffcfd2017078332bc9aaa00c14
YOUPIDOK <58979097+YOUPIDOK@users.noreply.github.com>
2023-01-31 17:04:21 +0100

    Fix form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
+use App\Enum\UserRoleEnum;
+use App\Form\StudentType;
+use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -32,5 +38,42 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/creer-un-compte-etudiant', name: 'register_student')]
+    public function registerStudent(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        StudentRepository $studentRepository
+    ): Response
+    {
+        if ($this->getUser()) {
+            $this->addFlash('warning', 'Vous êtes déja connecté.');
+
+            return $this->redirectToRoute('home');
+        }
+
+        $student = new Student();
+        $form = $this->createForm(StudentType::class, $student, ['required_password' => true]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('user')->get('plainPassword')->getData();
+            if ($plainPassword != null) {
+                $hashedPassword = $passwordHasher->hashPassword($student->getUser(), $plainPassword);
+                $student->getUser()->setPassword($hashedPassword);
+            }
+
+            $student->getUser()->addRole(UserRoleEnum::ROLE_STUDENT);
+
+            $studentRepository->save($student, true);
+
+            return $this->redirectToRoute('login', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('pages/register_student.html.twig', [
+            'student' => $student,
+            'form' => $form,
+        ]);
     }
 }
